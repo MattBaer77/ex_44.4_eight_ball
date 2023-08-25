df8b39192b0fbf9d77a25a965f1cc00b8e019d92
Matthew Baer <matt.baer77@gmail.com>
2023-08-25 00:33:00 -0400

    initial setup - files

---
+++ src/EightBall.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EightBall.css';
+
+const EightBall = (props) => {
+    return (
+        <>
+            <h2>Something</h2>
+        </>
+    )
+}
+
+export default EightBall;
