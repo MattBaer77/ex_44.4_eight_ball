3f92466cd1ffaede10cfc835f679ddd33ce1f1cc
Matthew Baer <matt.baer77@gmail.com>
2023-08-25 00:58:08 -0400

    updated css

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import './EightBall.css';
 
-const EightBall = (props) => {
+const EightBall = ({answers}) => {
     return (
-        <>
-            <h2>Something</h2>
-        </>
+
+        <div className='EightBall'>
+
+            <p className='EightBall-text'>Think Of A Question</p>
+
+        </div>
     )
 }
+
+EightBall.defaultProps = {answers: [
+
+    { msg: "It is certain.", color: "green" },
+    { msg: "It is decidedly so.", color: "green" },
+    { msg: "Without a doubt.", color: "green" },
+    { msg: "Yes - definitely.", color: "green" },
+    { msg: "You may rely on it.", color: "green" },
+    { msg: "As I see it, yes.", color: "green" },
+    { msg: "Most likely.", color: "green" },
+    { msg: "Outlook good.", color: "green" },
+    { msg: "Yes.", color: "green" },
+    { msg: "Signs point to yes.", color: "goldenrod" },
+    { msg: "Reply hazy, try again.", color: "goldenrod" },
+    { msg: "Ask again later.", color: "goldenrod" },
+    { msg: "Better not tell you now.", color: "goldenrod" },
+    { msg: "Cannot predict now.", color: "goldenrod" },
+    { msg: "Concentrate and ask again.", color: "goldenrod" },
+    { msg: "Don't count on it.", color: "red" },
+    { msg: "My reply is no.", color: "red" },
+    { msg: "My sources say no.", color: "red" },
+    { msg: "Outlook not so good.", color: "red" },
+    { msg: "Very doubtful.", color: "red" }
+
+]}
 
 export default EightBall;
